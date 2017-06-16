4aa77638eb6cc6f5f96a9900533584ab77665214
Kanna <naveench.cs@gmail.com>
2017-06-16 14:19:26 -0400

    Removed unnecessary test cases and updated FramesTest.java by removing some non working test case.

---
+++ src/test/java/demo/InterrogationTestSuite.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Kanna on 6/15/2017.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AlertsTest.class, CookieTest.class,UserInteractions.class})
+public class InterrogationTestSuite {
+}
